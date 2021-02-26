af644d65e9de7eee0dbec979781f940bb4f53a75
Ankur Gupta <rukna1000@gmail.com>
2021-02-26 10:38:28 -0500

    Add starting position + evaluator

--- scripts/quad_walk.py
+++ scripts/quad_walk.py
@@ -5,11 +5,31 @@ from gym_solo.envs import solo8v2vanilla_realtime
 from gym_solo.core import rewards
 from gym_solo import testing
 
+from tqdm import tqdm
+
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
+import math
 import threading
 import time
+
+
+epi_times, epi_rewards = [], []
+def episode_listener():
+  global epi_times
+  global epi_rewards
+  global end
+  curr_timestep = 0.
+  with tqdm(total=int(0.9 * math.floor((end - time.time())/args.dt)),
+            desc='Evaluating Episode') as t:
+    while time.time() < end:
+      epi_times.append(curr_timestep)
+      epi_rewards.append(walk_reward.compute())
+      curr_timestep += args.dt
+      t.update()
+      time.sleep(args.dt)
+  return epi_times, epi_rewards
 
 
 parser = argparse.ArgumentParser()
@@ -66,33 +86,38 @@ walk_reward.add_term(1, flat_reward)
 walk_reward.add_term(1, height_reward)
 walk_reward.add_term(1, speed_reward)
 
-joints = config.starting_joint_pos.copy()
 to_action = lambda d: [d[j] + config.starting_joint_pos[j] 
                         for j in env.joint_ordering]
+joints = {
+  'FL_HFE': np.pi / 4,
+  'FL_KFE': np.pi / 2,
+  'FL_ANKLE': 0,
+  'FR_HFE': -np.pi / 4,
+  'FR_KFE': -np.pi / 2,
+  'FR_ANKLE': 0,
+  'HL_HFE': np.pi / 4,
+  'HL_KFE': -np.pi / 2,
+  'HL_ANKLE': 0,
+  'HR_HFE': -np.pi / 4,
+  'HR_KFE': -np.pi / 2,
+  'HR_ANKLE': 0
+}
+env.step(to_action(joints))
+time.sleep(0.25)
 
-env.step([0] * len(env.joint_ordering))
 end = time.time() + args.length
-
-epi_times, epi_rewards = [], []
-def episode_listener():
-  global epi_times
-  global epi_rewards
-  curr_timestep = 0.
-  while time.time() < end:
-    epi_times.append(curr_timestep)
-    epi_rewards.append(walk_reward.compute())
-    curr_timestep += args.dt
-    time.sleep(args.dt)
-  return epi_times, epi_rewards
-
 scorer = threading.Thread(target=episode_listener)
 scorer.start()
+
 while time.time() < end:
-  # Gait goes here
+# joints['FR_HFE'] += -0.132
+# joints['FR_KFE'] += 0.132
+# env.step(to_action(joints))
   pass
 
+
 scorer.join()
-print(f'Score: {np.array(epi_rewards).mean()}')
+print(f'Average Score: {np.array(epi_rewards).mean()}')
 
 plt.plot(epi_times, epi_rewards)
 plt.title('Reward over Episode')
