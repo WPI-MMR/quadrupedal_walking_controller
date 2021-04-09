ac1faa613110aa5519dc8262435b2bd2119937d0
Ankur Gupta <rukna1000@gmail.com>
2021-04-09 10:55:31 -0400

    Create run task

--- quad_gaits/trot.py
+++ quad_gaits/trot.py
@@ -68,3 +68,68 @@ class Trot(Node):
   def FRHL_KFE(self, value):
     self.joints['FR_KFE'] = -value
     self.joints['HL_KFE'] = -value
+
+
+def trot(args=None):
+  """Run a super rudamentary, hardcoded trot in quadrupedal mode"""
+  rclpy.init(args=args)
+  trot = Trot()
+
+  # Initialize robot position
+  trot.joints = {
+    'FL_HFE': np.pi / 4,
+    'FL_KFE': np.pi / 2,
+    'FL_ANKLE': 0,
+    'FR_HFE': -np.pi / 4,
+    'FR_KFE': -np.pi / 2,
+    'FR_ANKLE': 0,
+    'HL_HFE': np.pi / 4,
+    'HL_KFE': -np.pi / 2,
+    'HL_ANKLE': 0,
+    'HR_HFE': -np.pi / 4,
+    'HR_KFE': -np.pi / 2,
+    'HR_ANKLE': 0,
+  }
+  trot.send_angles()
+
+  """
+  # Robot configuration values
+  trot_hip_launch = -0.8
+  trot_knee_launch = 1.4
+  trot_launch_dur = 0.25
+  trot_knee_clearance = 2
+  trot_clearance_dur = 0.1
+  trot_hip_step = -0.05
+  trot_knee_step = 1.5
+  trot_step_dur = 0.1
+
+  # while True:
+  # Get ready to launch FR and HL
+  trot.FLHR_HFE(trot_hip_launch)
+  trot.FLHR_KFE(trot_knee_launch)
+  trot.FRHL_KFE(trot_knee_clearance)
+  trot.send_angles()
+  time.sleep(trot_launch_dur)
+
+  # Make the FR and HL Movement
+  trot.FRHL_HFE(trot_hip_step)
+  trot.FRHL_KFE(trot_knee_step)
+  trot.send_angles()
+  time.sleep(trot_step_dur)
+
+  # Get ready to launch FL and HR
+  trot.FRHL_HFE(trot_hip_launch)
+  trot.FRHL_KFE(trot_knee_launch)
+  trot.FLHR_KFE(trot_knee_clearance)
+  trot.send_angles()
+  time.sleep(trot_launch_dur)
+
+  # Make the FL and HR Movement
+  trot.FLHR_HFE(trot_hip_step)
+  trot.FLHR_KFE(trot_knee_step)
+  trot.send_angles()
+  time.sleep(trot_step_dur)
+  """
+  
+  trot.destroy_node()
+  rclpy.shutdown()
